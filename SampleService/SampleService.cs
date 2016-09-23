7bf535bc907f5a9d8787a1c0760510668fcf2881
jcherry <joshcherry@quickenloans.com>
2016-09-23 15:28:47 -0400

    Added some random waits

--- SampleService/SampleService.cs
+++ SampleService/SampleService.cs
@@ -1,15 +1,18 @@
-﻿namespace SampleService
+﻿using System;
+using System.Threading;
+
+namespace SampleService
 {
     public class SampleService
     {
         public void Start()
         {
-
+            Thread.Sleep(new Random().Next(2000, 3000));
         }
 
         public void Stop()
         {
-
+            Thread.Sleep(new Random().Next(2000, 3000));
         }
     }
 }
